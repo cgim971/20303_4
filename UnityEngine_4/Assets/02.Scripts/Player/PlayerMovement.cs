be96096418b2a468dec431c89ac7afecf64ea995
ChanheeKim <cgim971@gmail.com>
2022-06-09 20:06:45 +0900

    jombie 에셋 추가 및 이동 해야 할 것 : OnGUI, 몬스터 스폰

--- UnityEngine_4/Assets/02.Scripts/Player/PlayerMovement.cs
+++ UnityEngine_4/Assets/02.Scripts/Player/PlayerMovement.cs
@@ -6,20 +6,37 @@ using UnityEngine.UI;
 public class PlayerMovement : MonoBehaviour
 {
 
-
     private CharacterController _characterController;
 
+    [Header("Movement Property")]
+    Vector3 moveAmount = Vector3.zero;
     public float _speed = 5f;
     public float _jumpPower = 8f;
     public float _gravity = 20f;
-    Vector3 moveAmount = Vector3.zero;
-
     float _vecGravity = 0;
-    bool _isJumping = false;
+    float h;
+    float v;
+
+    [Header("Animation Property")]
+    AnimationType _animationType = AnimationType.NONE;
+    Animator _animator;
+    bool _isAttacking = false;
+
+
+    enum AnimationType
+    {
+        NONE,
+        IDLE,
+        WALK,
+        RUN,
+        ATTACK,
+        DEATH
+    }
 
     private void Start()
     {
         _characterController = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -28,18 +45,52 @@ public class PlayerMovement : MonoBehaviour
 
         Move();
         SetGravity();
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            _animationType = AnimationType.ATTACK;
+        }
+
+        PlayAnimation();
+    }
+
+    void PlayAnimation()
+    {
+        if (_isAttacking) return;
+
+        switch (_animationType)
+        {
+            case AnimationType.IDLE:
+                _animator.SetBool("isMoving", false);
+                break;
+            case AnimationType.WALK:
+            case AnimationType.RUN:
+                _animator.SetFloat("H", h);
+                _animator.SetFloat("V", v);
+                _animator.SetBool("isMoving", true);
+                break;
+            case AnimationType.ATTACK:
+                _animator.SetTrigger("Attack");
+                _isAttacking = true;
+                break;
+            case AnimationType.DEATH:
+                _animator.SetTrigger("Death");
+                break;
+        }
     }
 
     void Move()
     {
-
         Vector3 forward = transform.forward;
         forward.y = 0;
 
         Vector3 right = new Vector3(forward.z, 0, -forward.x);
 
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        // animation Set
+        _animationType = (h == 0 && v == 0) ? AnimationType.IDLE : AnimationType.WALK;
 
         Vector3 targetDirection = h * right + v * forward;
         targetDirection.Normalize();
@@ -53,14 +104,12 @@ public class PlayerMovement : MonoBehaviour
 
     void SetGravity()
     {
-
-        if (_characterController.isGrounded)
-        {
-            _vecGravity = 0;
-        }
-        else
-        {
-            _vecGravity -= _gravity * Time.deltaTime;
-        }
+        if (_characterController.isGrounded) _vecGravity = 0;
+        else _vecGravity -= _gravity * Time.deltaTime;
+    }
+        
+    public void FinishAttack()
+    {
+        _isAttacking = false;
     }
 }
